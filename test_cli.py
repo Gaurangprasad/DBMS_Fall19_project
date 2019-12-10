e685e9e8f5668afb1adf781fecf6b232ea3a524c
Satej Sawant <satejs93@gmail.com>
2019-12-10 00:52:45 -0500

    Moved to functions

--- test_cli.py
+++ test_cli.py
@@ -10,6 +10,38 @@ import itertools
 from psql_structure import FoodViolationData
 from json_structure import InsuranceData
 
+def query1():
+    county = input('Enter county: ')
+            # license_codes = input('Enter license codes')
+    query = """ select premise_name,doing_business_as,address,zipcode, license_type_name from liquor_license, license_types
+    where county ilike '""" + county + """' AND
+    liquor_license.license_type_code = license_types.license_type_code
+    AND
+    license_types.license_type_code IN ('D','MI','CF') """
+            # Looking for estabishments that have the word BUFFET in their name and are in ALBANY
+    print(fd_vio.directQuery(query))
+def query2():
+    start_date = input('Enter start date(DD/MM/YYYY):')
+    end_date = input('Enter end date(DD/MM/YYYY):')
+    query = """select liquor_license.county,date_of_inspection,food_service_violations.violation_description, liquor_license.doing_business_as
+                        from food_service_operator,
+                            liquor_license,
+                            food_service_inspections,
+                            food_service_violations
+                        WHERE
+                        food_service_operator.operation_name = liquor_license.premise_name
+                        AND
+                        food_service_operator.nys_health_operation_id = food_service_inspections.nys_health_operation_id
+                        AND
+                        food_service_inspections.violation_item NOT IN ('None')
+                        AND
+                        food_service_inspections.date_of_inspection >= '""" + start_date + """' AND
+                            food_service_inspections.date_of_inspection <= '""" + end_date + """'
+                        AND
+                        food_service_violations.violation_item = food_service_inspections.violation_item;
+                        """
+    print(fd_vio.directQuery(query))
+
 if __name__ == "__main__":
     
     # Handling command line arguments
@@ -31,36 +63,9 @@ if __name__ == "__main__":
     
     if(parser['query'] is not None):
         if(int(parser['query']) == 1 ):
-            county = input('Enter county: ')
-            # license_codes = input('Enter license codes')
-            query = """ select premise_name,doing_business_as,address,zipcode, license_type_name from liquor_license, license_types
-    where county ilike '""" + county + """' AND
-    liquor_license.license_type_code = license_types.license_type_code
-    AND
-    license_types.license_type_code IN ('D','MI','CF') """
-            # Looking for estabishments that have the word BUFFET in their name and are in ALBANY
-            print(fd_vio.directQuery(query))
+            query1()
         if(int(parser['query']) == 2 ):
-            start_date = input('Enter start date(DD/MM/YYYY):')
-            end_date = input('Enter end date(DD/MM/YYYY):')
-            query = """select liquor_license.county,date_of_inspection,food_service_violations.violation_description, liquor_license.doing_business_as
-                        from food_service_operator,
-                            liquor_license,
-                            food_service_inspections,
-                            food_service_violations
-                        WHERE
-                        food_service_operator.operation_name = liquor_license.premise_name
-                        AND
-                        food_service_operator.nys_health_operation_id = food_service_inspections.nys_health_operation_id
-                        AND
-                        food_service_inspections.violation_item NOT IN ('None')
-                        AND
-                        food_service_inspections.date_of_inspection >= '""" + start_date + """' AND
-                            food_service_inspections.date_of_inspection <= '""" + end_date + """'
-                        AND
-                        food_service_violations.violation_item = food_service_inspections.violation_item;
-                        """
-            print(fd_vio.directQuery(query))
+            query2()
 
 
 
